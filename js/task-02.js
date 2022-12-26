36ff6d2aadfd47db20a37327a1bf09db26f071c5
MaksymchukA <nastena_1991@ukr.net>
2022-12-26 21:01:38 +0200

    did second task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.getElementById("ingredients");
+console.log(listEl);
+
+ingredients.forEach ((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ingredient;
+  itemEl.classList.add("item");
+  listEl.appendChild(itemEl);
+})
