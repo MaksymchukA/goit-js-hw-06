d099fd0d2d4cc3c9465a59fbb83c2ec60e42d377
MaksymchukA <nastena_1991@ukr.net>
2022-12-26 23:15:42 +0200

    did forth task

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+let counterValue = 0;
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+
+const incrementBtn = document.querySelector('[data-action="increment"]');
+
+const valueEl = document.querySelector('#value');
+
+decrementBtn.addEventListener('click', () => {
+    counterValue -= 1;
+    valueEl.textContent = counterValue;
+    return counterValue;
+});
+
+incrementBtn.addEventListener('click', () => {
+    counterValue += 1;
+    valueEl.textContent = counterValue;
+    return counterValue;
+});
