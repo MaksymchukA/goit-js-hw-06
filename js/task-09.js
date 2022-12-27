eb388df9710dbcc9d89c11f38cf59d0f801e88c8
MaksymchukA <nastena_1991@ukr.net>
2022-12-27 17:06:21 +0200

    did ninth task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const button = document.querySelector('.change-color');
+const spanColor = document.querySelector('span');
+
+button.addEventListener('click', () => {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+    spanColor.textContent = randomColor;
+})
